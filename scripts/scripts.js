5f2f99ff33744f502a2322a897ea74caaf40a4fa
baginskijakub <baginskijakub@icloud.com>
2022-06-02 11:51:01 +0200

    update - 2.06
    new functionalities added + navbar fixed

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -16,43 +16,59 @@ function changeNavbarToTransparent() {
     cartCounter.classList.add('cart-counter-default');
 }
 
+function hideNavbarLogo()
+    {
+        document.getElementsByClassName('logo-button')[0].style.opacity = "0";
+    }
+
+function showNavbarLogo()
+    {
+        document.getElementsByClassName('logo-button')[0].style.opacity = "1";
+    }
+
 function showNavbar()
 {
 
     let navbar = document.querySelector('.navbar-container');
 
     navbar.style.top = '0';
-
-    setTimeout(() => {
-        if (window.pageYOffset != 0)
-        {
-                    navbar.style.top = '-80px';
-        }
-    }, 5000);
-
 }
 
 
 var lastScrollTop;
 navbar = document.querySelector('.navbar-container');
-window.addEventListener('scroll', function () {
-    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-    if (scrollTop == 0){
-        changeNavbarToTransparent();
-    }
+window.addEventListener('scroll', function () {
     
-    if (scrollTop > lastScrollTop) {
-        navbar.style.top = '-80px';
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    var searchActive = document.getElementById('search-field').style.display
+
+    if (scrollTop > lastScrollTop && searchActive === "none") {
+        navbar.style.top = '-80px'; 
     }
-    
     else {
         showNavbar();
     }
-    if (scrollTop > 0) {
 
-        changeNavbarToWhite();
+    if (scrollTop == 0 && searchActive === "none"){
+        // changeNavbarToTransparent();
+        // hideNavbarLogo();
+        setTimeout(function()
+        {
+            var search = document.getElementById('search-field');
+            changeNavbarToTransparent();
+                hideNavbarLogo();
+
+        }, 200);
     }
+    if (scrollTop > 0) {
+        setTimeout(function()
+        {
+            changeNavbarToWhite();
+            showNavbarLogo();
+        }, 200);
+    }
+    
     lastScrollTop = scrollTop;
 });
 
